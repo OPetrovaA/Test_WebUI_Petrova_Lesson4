f212f2e33e44e8200f10304dbcf974ca71204e45
geekbPetrova <olga.alex.contact@mail.ru>
2022-04-14 23:38:15 +0300

    делаю вчерашний коммит к 4 ДЗ

--- src/test/java/AreaTest.java
+++ src/test/java/AreaTest.java
@@ -1,3 +1,17 @@
+import org.junit.*;
+import org.junit.jupiter.api.Assertions;
+
 public class AreaTest {
-    
+
+    @Test
+    public void Test(){
+    Integer side1 = 7;
+    Integer side2 = 8;
+    Integer side3 = 9;
+    double expResult = 26.832815729997478;
+    Integer s = (side1 + side2 + side3)/2;
+    double area = Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+    Assertions.assertEquals(expResult, area);
+    }
+
 }
